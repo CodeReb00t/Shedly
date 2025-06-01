31ed86571edcb477fef7c79cc692127fe1156859
devansh <devanshkg19@gmail.com>
2025-06-01 12:35:31 +0530

    Modal added

---
+++ client/src/app/projects/ModalNewProject/index.tsx
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+import {useCreateProjectMutation} from "@/state/api";
+import Modal from "@/components/Modal";
+
+type Props = {
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+const ModalNewProject = ({
+    isOpen,
+    onClose,}:Props) => {
+    const [createProject,{isLoading}] =useCreateProjectMutation();
+
+    const [projectName, setProjectName] = useState("");
+    const [description, setDescription] = useState("");
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
+
+    const handleSubmit = async () =>{
+        if(!projectName || !startDate || !endDate) return;
+        await createProject({
+            name: projectName,
+            description,
+            startDate,
+            endDate,
+        })
+    }
+
+    const isFormValid = () => {
+        return projectName && description && startDate && endDate;
+    }
+    const inputStyles="w-full rounded border border-gray-300 p-2 shadow-sm dark:border-dark-tertiary dark:bg-dark-tertiary dark:text-white dark:focus:outline-none"
+    return (
+        <Modal isOpen={isOpen} onClose={onClose} name='Create New Project'>
+            <form
+                className="mt-4 space-y-6"
+                onSubmit={(e)=>{
+                    e.preventDefault();
+                    handleSubmit()
+                }}>
+                <input
+                    type="text"
+                    className={inputStyles}
+                    placeholder="Project Name"
+                    value={projectName}
+                    onClick={(e)=>setProjectName(e.target.value)}
+                />
+                <textarea
+                    className={inputStyles}
+                    placeholder="Description"
+                    value={description}
+                    onClick={(e)=>setDescription(e.target.value)}
+                />
+                
+            </form>
+        </Modal>
+    );
+}
+
+export default ModalNewProject;
