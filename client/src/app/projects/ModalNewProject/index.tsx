f48fbeed9f75a76e3890bb3578d67afd434adc3a
devansh <devanshkg19@gmail.com>
2025-06-06 21:25:55 +0530

    Modal updated

--- client/src/app/projects/ModalNewProject/index.tsx
+++ client/src/app/projects/ModalNewProject/index.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {useCreateProjectMutation} from "@/state/api";
 import Modal from "@/components/Modal";
-
+import { formatISO } from "date-fns";
 type Props = {
     isOpen: boolean;
     onClose: () => void;
@@ -19,11 +19,13 @@ const ModalNewProject = ({
 
     const handleSubmit = async () =>{
         if(!projectName || !startDate || !endDate) return;
+        const formattedStartDate = formatISO(new Date(startDate),{representation: "complete"});
+        const formattedEndDate = formatISO(new Date(endDate),{representation: "complete"});
         await createProject({
             name: projectName,
             description,
-            startDate,
-            endDate,
+            startDate:formattedStartDate,
+            endDate:formattedEndDate    ,
         })
     }
 
@@ -44,15 +46,35 @@ const ModalNewProject = ({
                     className={inputStyles}
                     placeholder="Project Name"
                     value={projectName}
-                    onClick={(e)=>setProjectName(e.target.value)}
+                    onChange={(e)=>setProjectName(e.target.value)}
                 />
                 <textarea
                     className={inputStyles}
                     placeholder="Description"
                     value={description}
-                    onClick={(e)=>setDescription(e.target.value)}
+                    onChange={(e)=>setDescription(e.target.value)}
                 />
-                
+                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-2">
+                    <input
+                        type="date"
+                        className={inputStyles}
+                        value={startDate}
+                        onChange={(e)=>setStartDate(e.target.value)}
+                    />
+                    <input
+                        type="date"
+                        className={inputStyles}
+                        value={endDate}
+                        onChange={(e)=>setEndDate(e.target.value)}
+                    />
+                </div>
+                <button
+                    type="submit"
+                    className={`mt-4 flex w-full justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600 focus:offset-2 ${!isFormValid() || isLoading ?  "cursor-not-allowed opacity-50" : ""}`}
+                    disabled={!isFormValid() || isLoading}
+                >
+                    {isLoading ? "Creating... ": "Create Project"}
+                </button>
             </form>
         </Modal>
     );
